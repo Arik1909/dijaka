2618de157fb2b1c6a91d30d0224fc4d0d0b560da
Arik1909 <ariksamsonow05@gmail.com>
2026-07-19 11:56:18 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open('diia-cache').then((cache) => {
+    return cache.addAll(['/', '/index.html']);
+  }));
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then((response) => {
+    return response || fetch(event.request);
+  }));
+});
